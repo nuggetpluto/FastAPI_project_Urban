9d20ce798eb7f0287d66a3761f61cdf2753d6037
nuggetpluto <korshikov1501@gmail.com>
2024-09-27 14:47:37 +0300

    Валидация данных

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 from typing import Optional
-
+from typing import Annotated
 app = FastAPI()
 
 
 @app.get("/")
-async def welcome():
+def get_main_page():
     return "Главная страница!"
 
 
 @app.get("/user/admin")
-async def get_admin_page():
+def get_admin_page():
     return "Вы вошли как администратор!"
 
 
 
 @app.get("/user/{user_id}")
-async def get_user_number(user_id: int):
-    return f"Вы вошли как пользователь №{user_id}!"
+def read_user(user_id: Annotated[int, Path(description="Enter User ID", ge=1, le=100)]):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
 
-@app.get("/user")
-def read_user_info(username: Optional[str] = None, age: Optional[int] = None):
+
+@app.get("/user/{username}/{age}")
+def read_user_info(
+    username: Annotated[str, Path(description="Enter username", min_length=5, max_length=20)],
+    age: Annotated[int, Path(description="Enter age", ge=18, le=120)]
+):
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
