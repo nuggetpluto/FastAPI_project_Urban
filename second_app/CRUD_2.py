c7bb3014ad12eff4ab594f5eb90205ac7a4daf85
nuggetpluto <korshikov1501@gmail.com>
2024-09-29 14:08:11 +0300

    Шаблонизатор Jinja 2.

--- second_app/CRUD_2.py
+++ second_app/CRUD_2.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from typing import List
+from fastapi.templating import Jinja2Templates
 
 
 class User(BaseModel):
@@ -12,19 +14,30 @@ class User(BaseModel):
 app = FastAPI()
 
 users: List[User] = []
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/users", response_model=List[User])
-def get_users():
-    return users
+@app.get("/", response_class=HTMLResponse)
+def read_all_users(request: Request):
+    return templates.TemplateResponse("users.html", {"request": request, "users": users, "user": None})
 
 
-@app.post("/user/{username}/{age}", response_model=User)
+@app.get("/users/{user_id}", response_class=HTMLResponse)
+def read_user(request: Request, user_id: int):
+    user = next((user for user in users if user.id == user_id), None)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return templates.TemplateResponse("users.html", {"request": request, "users": users, "user": user})
+
+
+
+@app.post("/user/{username}/{age}")
 def create_user(username: str, age: int):
     user_id = 1 if not users else users[-1].id + 1
     new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
     return new_user
+
 
 
 @app.put("/user/{user_id}/{username}/{age}", response_model=User)
